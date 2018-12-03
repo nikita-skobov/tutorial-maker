1bdf69700eabeb707568674ad0e26a6a3082478d
nikita-skobov <nskobov.websites@gmail.com>
2018-12-02 22:04:02 -0600

    adds commandlinne to bin

--- bin/tutorial-maker.js
+++ bin/tutorial-maker.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const puppeteer = require('../lib/puppeteer')
+const commandLine = require('../lib/commandLine')
 
 const pup = new puppeteer({ width: 1000, height: 800 })
 pup.init()
@@ -8,3 +9,5 @@ pup.init()
 setTimeout(() => {
   pup.quit()
 }, 5000)
+
+const reader = new commandLine()
