b2f382ad2fa7f41291c36304c3f840d196865ca5
nikita-skobov <nskobov.websites@gmail.com>
2018-12-02 22:42:46 -0600

    adds basic setup via command prompts

--- bin/tutorial-maker.js
+++ bin/tutorial-maker.js
@@ -3,11 +3,61 @@
 const puppeteer = require('../lib/puppeteer')
 const commandLine = require('../lib/commandLine')
 
-const pup = new puppeteer({ width: 1000, height: 800 })
-pup.init()
 
-setTimeout(() => {
-  pup.quit()
-}, 5000)
+async function main() {
+  const width = 1600
+  const height = 800
 
-const reader = new commandLine()
+  const reader = new commandLine({ commands: ['hellp', 'butt', 'bad', 'bit'] })
+
+  let browser = null
+  let answer = await reader.ask(`Use default width and height: ${width} x ${height}? (Y or N)\n`)
+
+  const acceptedAnswers = ['yes', 'y', '']
+
+  if (acceptedAnswers.indexOf(answer.toLowerCase()) !== -1) {
+    // answer exists in acceptedAnswers list, proceed with defaults
+    browser = new puppeteer({ width, height })
+    browser.init()
+    
+    setTimeout(() => {
+      browser.quit()
+    }, 5000)
+  } else {
+    let proceed = false
+    let userWidth = null
+    let userHeight = null
+
+    while (!proceed) {
+      answer = await reader.ask('Enter desired width: \n')
+      userWidth = parseInt(answer, 10)
+      if (!Number.isNaN(userWidth)) {
+        // only proceed if width is a number
+        proceed = true
+      } else {
+        console.log(`${answer} is an invalid width\n`)
+      }
+    }
+  
+    proceed = false
+    while (!proceed) {
+      answer = await reader.ask('Enter desired height: \n')
+      userHeight = parseInt(answer, 10)
+      if (!Number.isNaN(userHeight)) {
+        // only proceed if height is a number
+        proceed = true
+      } else {
+        console.log(`${answer} is an invalid height\n`)
+      }
+    }
+
+    browser = new puppeteer({ width: userWidth, height: userHeight })
+    browser.init()
+    
+    setTimeout(() => {
+      browser.quit()
+    }, 5000)
+  }
+}
+
+main()
